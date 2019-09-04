be194ed9e3fe38e7f34d3be301704fa35b97cece
gmb9 <gmb9@humboldt.edu>
2019-09-04 15:54:04 -0700

    Added a nice GUI to bottom and top of screen

--- projects/TextEditor/src/TextEdit/TextEdit/main.cpp
+++ projects/TextEditor/src/TextEdit/TextEdit/main.cpp
@@ -27,6 +27,7 @@ int main(void)
 	curs_set(0);
 
 	//fun stuff happens here
+
 	for (int i = 0; i < num_cols; i++)
 	{
 		//top border
@@ -34,6 +35,44 @@ int main(void)
 
 		//bottom border
 		mvaddch(num_rows - 1, i, ACS_CKBOARD);
+	}
+
+	//top gui bar
+	mvaddstr(1, 1, "|  File |  Edit  |  Options  |  Buffers  |  Tools  |  Help  |");
+
+	//bottom gui bars
+	mvaddstr(num_rows - 3, 1, "|  ^G Get Help  |  ^O Write Out  |  ^R Read File  |  ^Y Prev Page  |  ^C Cur Pos   |");
+	mvaddstr(num_rows - 2, 1, "|  ^X Exit      |  ^J Justify    |  ^W Where Is   |  ^V Next Page  |  ^T To Spell  |");
+
+	
+	for (int i = 62; i < num_cols; i++)
+	{
+		//finish line next to top gui bar
+		mvaddch(1, i, ACS_CKBOARD);
+	}
+
+	for (int i = 85; i < num_cols; i++)
+	{
+		//finish line next to bottom gui bars
+		mvaddch(num_rows - 3, i, ACS_CKBOARD);
+	}
+
+	for (int i = 85; i < num_cols; i++)
+	{
+		//finish line next to bottom gui bars
+		mvaddch(num_rows - 2, i, ACS_CKBOARD);
+	}
+
+	for (int i = 0; i < num_cols; i++)
+	{
+		//line under top bar
+		mvaddch(2, i, ACS_HLINE);
+	}
+
+	for (int i = 0; i < num_cols; i++)
+	{
+		//line above bottom bar
+		mvaddch(num_rows - 4, i, ACS_HLINE);
 	}
 
 	for (int i = 0; i < num_rows; i++)
